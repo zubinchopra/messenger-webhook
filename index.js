f826e46ac3640b0ede5838938ce21865f15adb14
zubinchopra <zubinc@uw.edu>
2017-10-22 00:29:10 -0700

    Changed index.js

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-'use strict';
-
 const 
     express = require('express'),
     bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json());
+    request = require('request'),
+    app = express();
 
-app.set(process.env.PORT || 1337);
+app.set('port', (process.env.PORT || 5000));
 
-app.post('/webhook', (req, res) => {
+app.use(bodyParser.urlencoded({enxtended: false}));
+app.use(bodyParser.json());
 
-    let body = req.body;
-
-    if(body.object == 'page') {
-
-        body.entry.forEach(function(entry) {
-            let webhookEvent = entry.messaging[0];
-            console.log(webhookEvent);
-        });
-
-        res.status(200).send('EVENT_RECIEVED');
-    } else {
-        res.sendStatus(404);
-    }
-
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.get('/webhook', (req, res) => {
-
-    let VERIFY_TOKEN = "SAFEPATH";
-
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    if(mode && token) {
-
-        if(mode == 'subscribe' && token == VERIFY_TOKEN) {
-            console.log('WEBHOOK_RECEIVED');
-            res.status(200).send(challenge);
-        } else {
-            res.sendStatus(403);
-        }
-
+app.get('/webhook/', (req, res) => {
+    if(req.query['hub.verify_token'] === 'SAFEPATH') {
+        res.send(req.query['hub.challenge'])
     }
-
+    res.send('No entry');
 });
+
+// app.post('/webhook', (req, res) => {
+
+//     let body = req.body;
+
+//     if(body.object == 'page') {
+
+//         body.entry.forEach(function(entry) {
+//             let webhookEvent = entry.messaging[0];
+//             console.log(webhookEvent);
+//         });
+
+//         res.status(200).send('EVENT_RECIEVED');
+//     } else {
+//         res.sendStatus(404);
+//     }
+
+// });
+
+
 
 app.listen(app.get('port'), () => {
     console.log('running on port', app.get('port'));
