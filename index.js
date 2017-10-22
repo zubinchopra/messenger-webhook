8c9c2d8494b0a742811558d719df26068c062efc
zubinchopra <zubinc@uw.edu>
2017-10-22 02:16:47 -0700

    changed index.js

--- index.js
+++ index.js
@@ -24,40 +24,39 @@ app.get('/webhook/', (req, res) => {
 // Accepts POST requests at /webhook endpoint
 app.post('/webhook', (req, res) => {  
     
-      // Parse the request body from the POST
-      let body = req.body;
+    // Parse the request body from the POST
+    let body = req.body;
     
-      // Check the webhook event is from a Page subscription
-      if (body.object === 'page') {
+    // Check the webhook event is from a Page subscription
+    if (body.object === 'page') {
     
         body.entry.forEach(function(entry) {
     
-          // Gets the body of the webhook event
-          let webhook_event = entry.messaging[0];
-          console.log(webhook_event);
-    
-    
-          // Get the sender PSID
-          let sender_psid = webhook_event.sender.id;
-          console.log('Sender ID: ' + sender_psid);
-    
-          // Check if the event is a message or postback and
-          // pass the event to the appropriate handler function
-          if (webhook_event.message) {
-            handleMessage(sender_psid, webhook_event.message);        
-          } else if (webhook_event.postback) {
-            
-            handlePostback(sender_psid, webhook_event.postback);
-          }
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender ID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);        
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
           
         });
         // Return a '200 OK' response to all events
         res.status(200).send('EVENT_RECEIVED');
     
-      } else {
+    } else {
         // Return a '404 Not Found' if event is not from a page subscription
         res.sendStatus(404);
-      }
+    }
     
 });
 
@@ -72,75 +71,78 @@ function handleMessage(sender_psid, received_message) {
         "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
       }
     } else if (received_message.attachments) {
-        // Get the URL of the message attachment
-        let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-          "attachment": {
-            "type": "template",
-            "payload": {
-              "template_type": "generic",
-              "elements": [{
-                "title": "Is this the right picture?",
-                "subtitle": "Tap a button to answer.",
-                "image_url": attachment_url,
-                "buttons": [
-                  {
-                    "type": "postback",
-                    "title": "Yes!",
-                    "payload": "yes",
-                  },
-                  {
-                    "type": "postback",
-                    "title": "No!",
-                    "payload": "no",
-                  }
-                ],
-              }]
-            }
+      // Get the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+      response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Is this the right picture?",
+              "subtitle": "Tap a button to answer.",
+              "image_url": attachment_url,
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Yes!",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "No!",
+                  "payload": "no",
+                }
+              ],
+            }]
           }
         }
-      } 
+      }
+    } 
+    
     // Send the response message
     callSendAPI(sender_psid, response);    
 }
 
-function handlePostback(sender_psid, received_postback) {
-    console.log('ok')
-     let response;
-    // Get the payload for the postback
-    let payload = received_postback.payload;
+// function handlePostback(sender_psid, received_postback) {
+//     console.log('ok')
+//      let response;
+//     // Get the payload for the postback
+//     let payload = received_postback.payload;
   
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-      response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-      response = { "text": "Oops, try sending another image." }
-    }
-    // Send the message to acknowledge the postback
-    callSendAPI(sender_psid, response);
-}
+//     // Set the response based on the postback payload
+//     if (payload === 'yes') {
+//       response = { "text": "Thanks!" }
+//     } else if (payload === 'no') {
+//       response = { "text": "Oops, try sending another image." }
+//     }
+//     // Send the message to acknowledge the postback
+//     callSendAPI(sender_psid, response);
+// }
 
-function callSendAPI(sender_psid, response) {
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    }
-
-    request({
-        "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": {"access_token": access},
-        "method": "POST",
-        "json": request_body
-    }, (err, res, body) => {
-        if(!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
-}
+// function callSendAPI(sender_psid, response) {
+//     // Construct the message body
+//     let request_body = {
+//       "recipient": {
+//         "id": sender_psid
+//       },
+//       "message": response
+//     }
+  
+//     // Send the HTTP request to the Messenger Platform
+//     request({
+//       "uri": "https://graph.facebook.com/v2.6/me/messages",
+//       "qs": { "access_token": PAGE_ACCESS_TOKEN },
+//       "method": "POST",
+//       "json": request_body
+//     }, (err, res, body) => {
+//       if (!err) {
+//         console.log('message sent!')
+//       } else {
+//         console.error("Unable to send message:" + err);
+//       }
+//     }); 
+// }
 
 
 
