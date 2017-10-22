e40575e432a4cb34ae0a85a6f510f9896bf8787d
zubinchopra <zubinc@uw.edu>
2017-10-22 02:39:34 -0700

    changed files

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/webhook/', (req, res) => {
-    if(req.query['hub.verify_token'] == token) {
+    if(req.query['hub.verify_token'] === token) {
         res.send(req.query['hub.challenge'])
     }
     res.send('No entry');
@@ -35,12 +35,12 @@ app.post('/webhook', (req, res) => {
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
-
-
+    
+    
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender ID: ' + sender_psid);
-
+    
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
@@ -104,45 +104,45 @@ function handleMessage(sender_psid, received_message) {
     callSendAPI(sender_psid, response);    
 }
 
-// function handlePostback(sender_psid, received_postback) {
-//     console.log('ok')
-//      let response;
-//     // Get the payload for the postback
-//     let payload = received_postback.payload;
+function handlePostback(sender_psid, received_postback) {
+    console.log('ok')
+     let response;
+    // Get the payload for the postback
+    let payload = received_postback.payload;
   
-//     // Set the response based on the postback payload
-//     if (payload === 'yes') {
-//       response = { "text": "Thanks!" }
-//     } else if (payload === 'no') {
-//       response = { "text": "Oops, try sending another image." }
-//     }
-//     // Send the message to acknowledge the postback
-//     callSendAPI(sender_psid, response);
-// }
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+}
 
-// function callSendAPI(sender_psid, response) {
-//     // Construct the message body
-//     let request_body = {
-//       "recipient": {
-//         "id": sender_psid
-//       },
-//       "message": response
-//     }
+function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
   
-//     // Send the HTTP request to the Messenger Platform
-//     request({
-//       "uri": "https://graph.facebook.com/v2.6/me/messages",
-//       "qs": { "access_token": PAGE_ACCESS_TOKEN },
-//       "method": "POST",
-//       "json": request_body
-//     }, (err, res, body) => {
-//       if (!err) {
-//         console.log('message sent!')
-//       } else {
-//         console.error("Unable to send message:" + err);
-//       }
-//     }); 
-// }
+    // Send the HTTP request to the Messenger Platform
+    request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": access },
+      "method": "POST",
+      "json": request_body
+    }, (err, res, body) => {
+      if (!err) {
+        console.log('message sent!')
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }); 
+  }
 
 
 
