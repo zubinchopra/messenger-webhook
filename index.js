2ae6ec395343304465e5553407feca97576d0409
zubinchopra <zubinc@uw.edu>
2017-10-22 04:16:48 -0700

    changed index.html

--- index.js
+++ index.js
@@ -79,10 +79,11 @@ function handleMessage(sender_psid, received_message) {
         if(messageText.includes("hey" || "hi")) {
             responseText = "Hey there! How can I help you?"
         }
+        else if(!messageText.includes("to home")){
+            response.text = "Here are the directions";
+        }
         else if(!messageText.includes("to")) {
             responseText = "Sure! Give me the address";
-        } else {
-            responseText = "Here are the directions";
         }
         response.text = responseText;
     }
