d8c0584f0204d6c7f2b47bdbc5d15be881f89ee7
zubinchopra <zubinc@uw.edu>
2017-10-22 01:18:26 -0700

    added callsendapi

--- index.js
+++ index.js
@@ -76,6 +76,28 @@ function handleMessage(sender_psid, received_message) {
     callSendAPI(sender_psid, response);    
 }
 
+function callSendAPI(sender_psid, response) {
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {"access_token": access},
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if(!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+
 
 
 app.listen(app.get('port'), () => {
