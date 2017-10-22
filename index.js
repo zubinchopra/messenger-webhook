4feacda43d1eab2013b3e5efa5987ee3bc59eb33
zubinchopra <zubinc@uw.edu>
2017-10-22 03:34:36 -0700

    changed response

--- index.js
+++ index.js
@@ -68,11 +68,14 @@ function handleMessage(sender_psid, received_message) {
     if (received_message.text) {    
       // Create the payload for a basic text message, which
       // will be added to the body of our request to the Send API
+      
+      var messageText = received_message.text;
+
       response = {
-        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+        "text": `You sent the message:` + messageText + `. Now send me an attachment!`
       }
     }
-    
+
     // Send the response message
     callSendAPI(sender_psid, response);    
 }
